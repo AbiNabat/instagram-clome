5305195eda31036877effe3c5851378b5e37160b
AbiNabat <154244797+AbiNabat@users.noreply.github.com>
2024-02-01 21:10:19 +0300

    home page1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
 
 export default function Header() {
     return (
         <>
             <div className='flex items-center justify-between max-w-6xl'>
-            {/* Left */}
+
             <div className=' relative cursor-pointer'>
                 <Image src='/logo.png'
                        alt={'logo'}
@@ -13,13 +14,16 @@ export default function Header() {
                        className='object-contain'
                        priority />
             </div>
+                <div className='relative mt-1'>
+                <div className='absolute top-2'>
+                    <CiSearch className='h-5 text-gray-500 '/>
+                </div>
+                <input type='text' placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
+                </div>
             <div>
                <h1>Right side</h1>
             </div>
-            {/* Middle */}
 
-
-            {/* Right */}
             </div>
         </>
     )
